7713a7be91ac768707c7e2c66e962c7a2e26b4e2
Tuku-Webian <tukuwebian@gmail.com>
2022-11-07 23:01:00 +0600

    Navbar HomePage Footer create

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,31 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#6df954",
+
+          "secondary": "#b54827",
+
+          "accent": "#79a521",
+
+          "neutral": "#29232F",
+
+          "base-100": "#F9F9FB",
+
+          "info": "#6786E9",
+
+          "success": "#6BEBA7",
+
+          "warning": "#F2CE40",
+
+          "error": "#E14C6C",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
