8f316f997964c4a8b8d2e155793d8378ed718905
Tuku-Webian <tukuwebian@gmail.com>
2022-11-10 01:54:53 +0600

    Environment var used

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyCbY1yUQNuSQNZFOLHBgw20sXlqwdshsH4",
-  authDomain: "saad-dentistry.firebaseapp.com",
-  projectId: "saad-dentistry",
-  storageBucket: "saad-dentistry.appspot.com",
-  messagingSenderId: "955988604182",
-  appId: "1:955988604182:web:d7b45b0ab61a4259ad0c37"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
